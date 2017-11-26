bddef187929168215e7d89e5c7c3ca4b5fa9f25f
Keller Johnson <kjlt4412@gmail.com>
2017-11-26 13:17:47 -0700

    update

--- src/components/questionCategory/MultipleChoice.js
+++ src/components/questionCategory/MultipleChoice.js
@@ -3,6 +3,7 @@ import {Route, Link} from 'react-router-dom';
 import './../../styles/multipleChoice.css';
 import NewSurvey from './../NewSurvey';
 
+
 class MultipleChoice extends Component{
     render() {
         return (
@@ -19,4 +20,26 @@ class MultipleChoice extends Component{
     }
 }
 
+
 export default MultipleChoice;
+
+// Survey.Survey.cssType = "bootstrap";
+// Survey.defaultBootstrapCss.navigationButton = "btn btn-green";
+
+// var json =  { questions: [
+//      {name:"name", type:"text", title: "Please enter your name:", placeHolder:"Jon Snow", isRequired: true},
+//      {name:"birthdate", type:"text", inputType:"date", title: "Your birthdate:", isRequired: true},
+//      {name:"color", type:"text", inputType:"color", title: "Your favorite color:"},
+//      {name:"email", type:"text", inputType:"email", title: "Your e-mail:", placeHolder:"jon.snow@nightwatch.org", isRequired: true, validators: [{type:"email"}]}
+// ]};
+
+// window.survey = new Survey.Model(json);
+
+
+// survey.onComplete.add(function(result) {
+//     document.querySelector('#surveyResult').innerHTML = "result: " + JSON.stringify(result.data);
+// });
+
+
+// ReactDOM.render(<Survey.Survey model ={ survey}  />, document.getElementById("surveyElement"));
+
